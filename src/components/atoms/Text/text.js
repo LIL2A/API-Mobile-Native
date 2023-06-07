f4d35f9614b23f5f5c01f7480d278929bc89277c
Marilia Branco <dinmarilia@outlook.com>
2023-06-06 22:21:08 -0300

    Adicionando fontes

--- src/components/atoms/Text/text.js
+++ src/components/atoms/Text/text.js
@@ -11,8 +11,8 @@ export const TextStyle = ({children}) => {
 
 const style = StyleSheet.create({
     titleText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 10
+        fontSize: 22,
+        marginTop: 10,
+        fontFamily: 'SourceSansPro_700Bold',
     }
 })
